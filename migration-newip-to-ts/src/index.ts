ad6e50460e301c6820dfbc5fa01a32e83b572097
Igor Bespalov <igorbespalov89@gmail.com>
2024-02-28 23:41:40 +0300

    feat: add change theme

--- migration-newip-to-ts/src/index.ts
+++ migration-newip-to-ts/src/index.ts
@@ -3,3 +3,19 @@ import './global.css';
 
 const app = new App();
 app.start();
+
+const changeThemeButton: Element | null = document.querySelector('.change-theme');
+
+if (changeThemeButton) {
+    changeThemeButton.addEventListener('click', changeTheme);
+}
+
+function changeTheme(): void {
+    const doc: HTMLElement = document.documentElement;
+
+    if (doc.getAttribute('data-theme') === 'light') {
+        doc.setAttribute('data-theme', 'dark');
+    } else {
+        doc.setAttribute('data-theme', 'light');
+    }
+}
